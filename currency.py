5f0ca9587543cbcd72200b12ab33137a53a413c1
Ei Sin <einge174@gmail.com>
2025-03-03 09:15:03 +0000

    Code tidying up

--- currency.py
+++ currency.py
@@ -4,30 +4,31 @@ import requests
 def get_rate(from_currency, to_currency, api_key):
     url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{from_currency}"
     response = requests.get(url)
-    print (response.status_code)
+    # print (response.status_code)
 
     if response.status_code == 200:
         data = response.json()
-        print (data['conversion_rates'])
-    
+       
         if (data['result']== "success") :
             rate = data['conversion_rates']
             to_currency_rate = rate.get(to_currency)
-            print (to_currency_rate)
+            print (f"Today rate is {to_currency_rate} of {data['time_last_update_utc']}")
             return to_currency_rate
         else :
             print ("Error with API Key")
             return None 
     
+    else :
+        print ("Failed to retrieve Exchange Reate data")
 
 # function to covert the currency from the amount that user input
 def convert_currency(amount, from_currency, to_currency, api_key):
     rate = get_rate(from_currency, to_currency, api_key)
-    print (rate)
+    # print (rate)
 
     if rate:
         converted_amount = amount * rate
-        print (f" {amount} {from_currency} of the converted amount is:  {converted_amount:.2f}{to_currency}")
+        print (f" {amount} {from_currency} of the converted amount is:  {converted_amount:.2f} {to_currency}")
 
     else:
         print("Failed")
