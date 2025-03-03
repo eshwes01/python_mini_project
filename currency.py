68ea42a13eb50f124d94a011ae38549846cbc0ec
Ei Sin <einge174@gmail.com>
2025-03-03 08:06:35 +0000

    convert_currency function added

--- currency.py
+++ currency.py
@@ -1,12 +1,16 @@
 import requests
 
-
+# retriving exchange rate with api key from the remote url 
 def get_rate(from_currency, to_currency, api_key):
     url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{from_currency}"
     response = requests.get(url)
     print (response.status_code)
 
-
+    if response.status_code == 200:
+        data = response.json()
+        print (data['conversion_rates'])
+        
+# function to covert the currency from the amount that user input
 def convert_currency(amount, from_currency, to_currency, api_key):
     rate = get_rate(from_currency, to_currency, api_key)
     print (rate)
@@ -18,15 +22,13 @@ def convert_currency(amount, from_currency, to_currency, api_key):
     else:
         print("Failed")
 
-
-   
-
+# main 
 if __name__ == "__main__":
             api_key = "a23c88406575a6194ee8921e"         
-            from_currency = input ("Type currency you want to change from e.g USD or GBP: ")
-            to_currency = input (" Add currency you want to change to e.g USD or GBP : ")
+            from_currency = input ("Type currency you want to change from e.g USD ,GBP ,AUD , : ")
+            to_currency = input (" Type currency you want to change to e.g USD or GBP : ")
             amount = input (" Enter the amount : ")
-
+            convert_currency(amount, from_currency,to_currency,api_key)
 
             
             
